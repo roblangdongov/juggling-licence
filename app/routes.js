88e6021c7942aa8d2ed3b29865e2798c2c554c14
robert.langdon1@digital.homeoffice.gov.uk <robert.langdon1@digital.homeoffice.gov.uk>
2023-11-13 15:09:51 +0000

    Change answers takes user back to the Check answers page

--- app/routes.js
+++ app/routes.js
@@ -8,33 +8,59 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 router.post('/new/email-address', (req, res) => {
+    if(req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+    } else {
     res.redirect('/new/code')
+    }
 })
 
 router.post('/new/code', (req, res) => {
+    if(req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+    } else {
     res.redirect('/new/name')
+    }
 })
 
 router.post('/new/name', (req, res) => {
-    res.redirect('/new/country')
+    if(req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+    } else {
+        res.redirect('/new/country')
+    }
 })
 
 router.post('/new/country', (req, res) => {
+    if(req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+    } else {
     res.redirect('/new/juggling-balls')
+    }
 })
 
 router.post('/new/juggling-balls', (req, res) => {    
     if(req.body.new.numberOfBalls == "None - I cannot juggle") {
         res.redirect('/new/not-eligible')
-    } else {
+    } else if(req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+    } else { 
         res.redirect('/new/juggling-trick')
     }
 })
 
 router.post('/new/juggling-trick', (req, res) => {
+    if(req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+    } else {
     res.redirect('/new/juggling-objects')
+    }
 })
 
 router.post('/new/juggling-objects', (req, res) => {
+    if(req.query.returnUrl) {
+        res.redirect(req.query.returnUrl)
+    } else {
     res.redirect('/new/check')
+    }
 })
